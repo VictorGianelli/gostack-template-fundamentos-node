0a128d2e415d5c43069742acb25d09ae57423cac
Victor Gianelli <victorgnll@gmail.com>
2020-08-26 20:50:29 -0300

    Challenge done

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,33 @@
 import { Router } from 'express';
+// import {} from 'da'
+import Transaction from '../models/Transaction'
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
+// interface Transaction {
+//   id: string,
+//     title: string,
+//     value: number,
+//     type: string,
+// }
 const transactionRouter = Router();
+const transactionsRepository = new TransactionsRepository();
 
-// const transactionsRepository = new TransactionsRepository();
+// const transaction: Transaction[] = [];
+
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+
+    const transaction = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transaction,
+      balance,
+    })
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,6 +35,15 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
+    const { title, value, type } = request.body
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository
+    )
+
+    const transaction = createTransaction.execute({ title, value, type })
+
+    return response.json(transaction)
     // TODO
   } catch (err) {
     return response.status(400).json({ error: err.message });
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface requestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: requestDTO): Transaction {
+
+    const { total } = this.transactionsRepository.getBalance()
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You don´t have enough money');
+    }
+
+    const eachTransaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    })
+
+    return eachTransaction;
   }
 }
 
